9b8f94a8514dc8bb7b02997c9dd55e8836aaf462
sfturing <sfturing@gmail.com>
2017-04-27 22:13:09 +0800

    MergingArrays

---
+++ algorithms/src/arrays/MergingArrays.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * @author sfturing
+ * 
+ */
+public class MergingArrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr1[] = {1,2,3,4,5};
+		int arr2[] = {9,8,7,6};
+		int arr[] = new int [arr1.length + arr2.length];
+		System.arraycopy(arr1, 0, arr, 0, arr1.length);
+		System.arraycopy(arr2, 0, arr, arr1.length, arr2.length);
+		System.out.println(Arrays.toString(arr));
+	}
+}

c1c0aad0c51771a148ec95a7a090ad603b702710
sfturing <sfturing@gmail.com>
2017-07-22 23:47:17 +0800

    5

---
+++ algorithms/src/cn/sfturing/offer/StackSolution.java
@@ -0,0 +1,15 @@
+package cn.sfturing.offer;
+/**
+ * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ * @author sfturing
+ *
+ * @date 2017年7月22日
+ */
+public class StackSolution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

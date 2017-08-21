c6e6373311b89fc2433df2c612d0a0635dc112ba
Xiaohao Shi <sfturing@gmail.com>
2017-08-21 18:08:24 +0800

    使用两个栈，实现队列操作。

--- algorithms/src/cn/sfturing/offer/StackSolution.java
+++ algorithms/src/cn/sfturing/offer/StackSolution.java
@@ -1,21 +1,31 @@
 package cn.sfturing.offer;
+
+import java.util.Stack;
+
 /**
  * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
- * @author sfturing
  *
+ * @author sfturing
  * @date 2017年7月22日
  */
 public class StackSolution {
-	/**
-	 * 
-	 * @author sfturing(史晓昊)
-	 * @E-mail sfturing@gmail.com
-	 * @date 2017年7月24日
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
 
-	}
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (stack1.empty() && stack2.empty()) {
+            throw new RuntimeException("Empty");
+        }
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
 
 }
